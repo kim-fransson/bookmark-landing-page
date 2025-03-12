5c19b1bf63503c2b152f915fd01c1073a0951af0
Kim Fransson <kim.fransson.22@gmail.com>
2025-03-12 14:17:34 +0100

    implemented link

---
+++ src/stories/Link.stories.js
@@ -0,0 +1,17 @@
+import Link from "../components/Link";
+
+export default {
+  component: Link,
+};
+
+export const LinkWithText = {
+  args: {
+    children: "Link with text",
+  },
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/design/9DjY1Y8aOWPHvRtICZKS0t/bookmark-landing-page?node-id=84-11&t=rGpx5JSqzH8DczMX-4",
+    },
+  },
+};
